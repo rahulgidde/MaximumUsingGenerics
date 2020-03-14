f9a99e26affee2797458e25e7265aeb5efb25662
rahulgidde <rahulgidde1212@gmail.com>
2020-03-14 17:35:54 +0530

    Added:Test For Second Number Is Maximum

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -7,9 +7,16 @@ public class MaximumNumberTest
     int result;
 
     @Test
-    public void givenIntegerNumbers_WhenMaximum_ThenReturnMaximumNumber()
+    public void givenIntegerNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
     {
-        result = number.getMaximumNumber(5, 10, 15);
+        result = number.getMaximumNumber(15, 10, 5);
+        Assert.assertEquals(15, result);
+    }
+
+    @Test
+    public void givenIntegerNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
+    {
+        result = number.getMaximumNumber(5, 15, 10);
         Assert.assertEquals(15, result);
     }
 }
