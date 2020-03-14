180500054ae0b733aeb23a9a9ec54d0732bc0b59
rahulgidde <rahulgidde1212@gmail.com>
2020-03-14 18:05:31 +0530

    Added:Test Case For Check First Float Number Is Maximum

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,9 +1,20 @@
 public class MaximumNumber
 {
-    //METHOD TO FIND MAXIMUM NUMBER
+    //METHOD TO FIND MAXIMUM INTEGER NUMBER
     public int getMaximumNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber)
     {
         int maximumNumber = firstNumber;
+        if(secondNumber.compareTo(maximumNumber) > 0)
+            maximumNumber = secondNumber;
+        if(thirdNumber.compareTo(maximumNumber) > 0)
+            maximumNumber = thirdNumber;
+        return maximumNumber;
+    }
+
+    //METHOD TO FIND MAXIMUM FLOAT NUMBER
+    public float getMaximumNumber(Float firstNumber, Float secondNumber, Float thirdNumber)
+    {
+        Float maximumNumber = firstNumber;
         if(secondNumber.compareTo(maximumNumber) > 0)
             maximumNumber = secondNumber;
         if(thirdNumber.compareTo(maximumNumber) > 0)
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -4,26 +4,34 @@ import org.junit.Test;
 public class MaximumNumberTest
 {
     MaximumNumber number = new MaximumNumber();
-    int result;
+    int result1;
+    Float result2;
 
     @Test
     public void givenIntegerNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
     {
-        result = number.getMaximumNumber(15, 10, 5);
-        Assert.assertEquals(15, result);
+        result1 = number.getMaximumNumber(15, 10, 5);
+        Assert.assertEquals(15, result1);
     }
 
     @Test
     public void givenIntegerNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
     {
-        result = number.getMaximumNumber(5, 15, 10);
-        Assert.assertEquals(15, result);
+        result1 = number.getMaximumNumber(5, 15, 10);
+        Assert.assertEquals(15, result1);
     }
 
     @Test
     public void givenIntegerNumbers_WhenThirdNumberIsMaximum_ThenReturnThirdNumber()
     {
-        result = number.getMaximumNumber(5, 10, 15);
-        Assert.assertEquals(15, result);
+        result1 = number.getMaximumNumber(5, 10, 15);
+        Assert.assertEquals(15, result1);
+    }
+
+    @Test
+    public void giveFloatNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
+    {
+        result2 = number.getMaximumNumber(15.4f, 10.4f, 5.4f);
+        Assert.assertEquals((Float)15.4f, result2);
     }
 }
