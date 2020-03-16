3fe42492df591c45000e0ea0112d52dde640c5f3
rahulgidde <rahulgidde1212@gmail.com>
2020-03-16 10:41:55 +0530

    Added:Default Constructor And Parametrized Constructor

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,5 +1,29 @@
 public class MaximumNumber<T extends Comparable<T>>
 {
+    private T firstValue;
+    private T secondValue;
+    private T thirdValue;
+
+    //DEFAULT CONSTRUCTOR
+    public MaximumNumber()
+    {
+
+    }
+
+    //PARAMETERISED CONSTRUCTOR
+    public MaximumNumber(T firstValue, T secondValue, T thirdValue)
+    {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    //METHOD TO CALL INTERNAL METHOD
+    public T getMaximum()
+    {
+        return getMaximum(firstValue,secondValue,thirdValue);
+    }
+
     //METHOD TO FIND MAXIMUM
     public <T extends Comparable<T>> T getMaximum(T firstNumber, T secondNumber, T thirdNumber)
     {
--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class MaximumNumberTest
 {
-    MaximumNumber maximum = new MaximumNumber();
+
     int result1;
     Float result2;
     String result3;
@@ -11,6 +11,7 @@ public class MaximumNumberTest
     @Test
     public void givenIntegerNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result1 = (int) maximum.getMaximum(15, 10, 5);
         Assert.assertEquals(15, result1);
     }
@@ -18,6 +19,7 @@ public class MaximumNumberTest
     @Test
     public void givenIntegerNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result1 = (int) maximum.getMaximum(5, 15, 10);
         Assert.assertEquals(15, result1);
     }
@@ -25,6 +27,7 @@ public class MaximumNumberTest
     @Test
     public void givenIntegerNumbers_WhenThirdNumberIsMaximum_ThenReturnThirdNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result1 = (int) maximum.getMaximum(5, 10, 15);
         Assert.assertEquals(15, result1);
     }
@@ -32,6 +35,7 @@ public class MaximumNumberTest
     @Test
     public void giveFloatNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result2 = (Float) maximum.getMaximum(15.4f, 10.4f, 5.4f);
         Assert.assertEquals((Float)15.4f, result2);
     }
@@ -39,6 +43,7 @@ public class MaximumNumberTest
     @Test
     public void giveFloatNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result2 = (Float) maximum.getMaximum(5.4f, 15.4f, 10.4f);
         Assert.assertEquals((Float)15.4f, result2);
     }
@@ -46,6 +51,7 @@ public class MaximumNumberTest
     @Test
     public void giveFloatNumbers_WhenThirdNumberIsMaximum_ThenReturnThirdNumber()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result2 = (Float) maximum.getMaximum(5.4f, 10.4f, 15.4f);
         Assert.assertEquals((Float)15.4f, result2);
     }
@@ -53,6 +59,7 @@ public class MaximumNumberTest
     @Test
     public void giveString_WhenFirstStringIsMaximum_ThenReturnFirstString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("welcome", "hello", "hi");
         Assert.assertEquals("welcome", result3);
     }
@@ -60,6 +67,7 @@ public class MaximumNumberTest
     @Test
     public void giveString_WhenSecondStringIsMaximum_ThenReturnSecondString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("hello", "welcome", "hi");
         Assert.assertEquals("welcome", result3);
     }
@@ -67,6 +75,7 @@ public class MaximumNumberTest
     @Test
     public void giveString_WhenThirdStringIsMaximum_ThenReturnThirdString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("hello", "hi", "welcome");
         Assert.assertEquals("welcome", result3);
     }
@@ -74,6 +83,7 @@ public class MaximumNumberTest
     @Test
     public void giveCapitalString_WhenGetMaximumString_ThenReturnMaximumString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("HELLO", "HI", "WELCOME");
         Assert.assertEquals("WELCOME", result3);
     }
@@ -81,6 +91,7 @@ public class MaximumNumberTest
     @Test
     public void giveNumberAsString_WhenGetMaximumString_ThenReturnMaximumString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("123", "231", "444");
         Assert.assertEquals("444", result3);
     }
@@ -88,7 +99,104 @@ public class MaximumNumberTest
     @Test
     public void giveStringCombination_WhenGetMaximumString_ThenReturnMaximumString()
     {
+        MaximumNumber maximum = new MaximumNumber();
         result3 = (String) maximum.getMaximum("ABC", "123", "abc");
+        Assert.assertEquals("abc", result3);
+    }
+
+    @Test
+    public void givenThreeIntegerNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(15, 5, 10);
+        result1 = (int) maximum.getMaximum();
+        Assert.assertEquals(15, result1);
+    }
+
+    @Test
+    public void givenThreeIntegerNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(5, 15, 10);
+        result1 = (int) maximum.getMaximum();
+        Assert.assertEquals(15, result1);
+    }
+
+    @Test
+    public void givenThreeIntegerNumbers_WhenThirdNumberIsMaximum_ThenReturnThirdNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(5, 10, 15);
+        result1 = (int) maximum.getMaximum();
+        Assert.assertEquals(15, result1);
+    }
+
+    @Test
+    public void giveThreeFloatNumbers_WhenFirstNumberIsMaximum_ThenReturnFirstNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(15.4f, 10.4f, 5.4f);
+        result2 = (Float) maximum.getMaximum();
+        Assert.assertEquals((Float)15.4f, result2);
+    }
+
+    @Test
+    public void giveThreeFloatNumbers_WhenSecondNumberIsMaximum_ThenReturnSecondNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(5.4f, 15.4f, 10.4f);
+        result2 = (Float) maximum.getMaximum();
+        Assert.assertEquals((Float)15.4f, result2);
+    }
+
+    @Test
+    public void giveThreeFloatNumbers_WhenThirdNumberIsMaximum_ThenReturnThirdNumber()
+    {
+        MaximumNumber maximum = new MaximumNumber(5.4f, 10.4f, 15.4f);
+        result2 = (Float) maximum.getMaximum();
+        Assert.assertEquals((Float)15.4f, result2);
+    }
+
+    @Test
+    public void giveThreeString_WhenFirstStringIsMaximum_ThenReturnFirstString()
+    {
+        MaximumNumber maximum = new MaximumNumber("welcome", "hello", "hi");
+        result3 = (String) maximum.getMaximum();
+        Assert.assertEquals("welcome", result3);
+    }
+
+    @Test
+    public void giveThreeString_WhenSecondStringIsMaximum_ThenReturnSecondString()
+    {
+        MaximumNumber maximum = new MaximumNumber("hello", "welcome", "hi");
+        result3 = (String) maximum.getMaximum();
+        Assert.assertEquals("welcome", result3);
+    }
+
+    @Test
+    public void giveThreeString_WhenThirdStringIsMaximum_ThenReturnThirdString()
+    {
+        MaximumNumber maximum = new MaximumNumber("hello", "hi", "welcome");
+        result3 = (String) maximum.getMaximum();
+        Assert.assertEquals("welcome", result3);
+    }
+
+    @Test
+    public void giveThreeCapitalString_WhenGetMaximumString_ThenReturnMaximumString()
+    {
+        MaximumNumber maximum = new MaximumNumber("HELLO", "HI", "WELCOME");
+        result3 = (String) maximum.getMaximum();
+        Assert.assertEquals("WELCOME", result3);
+    }
+
+    @Test
+    public void giveThreeNumberAsString_WhenGetMaximumString_ThenReturnMaximumString()
+    {
+        MaximumNumber maximum = new MaximumNumber("123", "231", "444");
+        result3 = (String) maximum.getMaximum();
+        Assert.assertEquals("444", result3);
+    }
+
+    @Test
+    public void giveThreeStringCombination_WhenGetMaximumString_ThenReturnMaximumString()
+    {
+        MaximumNumber maximum = new MaximumNumber("ABC", "123", "abc");
+        result3 = (String) maximum.getMaximum();
         Assert.assertEquals("abc", result3);
     }
 }
