67214959710a16f54f14f1bdc743d00199811bfc
rahulgidde <rahulgidde1212@gmail.com>
2020-03-16 09:13:38 +0530

    Refactor:All Methods To One Single Generic Method To Get Maximum Value

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -14,6 +14,6 @@ public class MaximumNumber
     //MAIN METHOD
     public static void main(String[] args)
     {
-        System.out.println("--------WELCOME--------");
+        System.out.println("---------WELCOME---------");
     }
 }
